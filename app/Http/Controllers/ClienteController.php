eaec822942093d6bdf8cff713c3a95fed1a698f8
gustavoamodei <gustavosouza407@gmail.com>
2021-07-19 01:06:27 -0300

    retirei model frasco de cliente

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUpdateClienteRequest;
 use App\Models\Cliente;
-use App\Models\Frasco;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Throwable;
